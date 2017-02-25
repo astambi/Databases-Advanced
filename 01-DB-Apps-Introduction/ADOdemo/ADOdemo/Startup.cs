05981c6e8edf2517c7c7f2d5b6b0ba0dd9e84fba
Anna <annsta@gmail.com>
2017-02-25 13:51:29 +0200

    DB Apps Introduction

---
+++ 01-DB-Apps-Introduction/ADOdemo/ADOdemo/Startup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOdemo
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(@"
+                Server=.;
+                Database=SoftUni;
+                Integrated Security=true");
+            connection.Open();
+
+            using (connection)
+            {
+                //SqlCommand ExecuteScalar
+                string query = "SELECT COUNT(*) FROM Employees";
+                SqlCommand command = new SqlCommand(query, connection);
+                int employeesCount = (int)command.ExecuteScalar();
+
+                Console.WriteLine("Employees count: {0}", employeesCount);
+
+                //SqlDataReader
+                Console.Write("Enter employeeId: ");
+                int employeeId = int.Parse(Console.ReadLine());
+
+                query = $@"SELECT EmployeeID, FirstName, LastName, Salary 
+                           FROM Employees 
+                           WHERE EmployeeId = {employeeId}";
+                command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                using (reader)
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                        Console.Write($"{reader.GetName(i),-15}");
+                    Console.WriteLine();
+
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                            Console.Write($"{reader[i],-15}");
+                        Console.WriteLine();
+                    }
+                }
+
+                // ExecuteNonQuery
+                Console.Write("Enter townName to be inserted: ");
+                string townName = Console.ReadLine();
+
+                query = $"INSERT INTO Towns (Name) VALUES ('{townName}')";
+                command = new SqlCommand(query, connection);
+                int insertedRows = command.ExecuteNonQuery();
+
+                Console.WriteLine($"Affected rows: {insertedRows}");
+
+                // ExecuteNonQuery + Prevent SQL Injection
+                Console.Write("Enter TownName to delete: ");
+                string townNameToDel = Console.ReadLine();                
+
+                query = $"DELETE FROM Towns WHERE Name LIKE @townName";
+                command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@townName", "%" + townNameToDel + "%");
+                int deletedRows = command.ExecuteNonQuery();
+
+                Console.WriteLine($"Affected rows: {deletedRows}");
+            }
+        }
+    }
+}
---
+++ 01-DB-Apps-Introduction/Examples/Examples/Startup.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection("Server=.;Database=SoftUni;Integrated Security=true");
+            connection.Open();
+
+            using (connection)
+            {
+                while (true)
+                {
+                    Console.Write("Enter command [list / details / search / searchALL / exit]: ");
+                    string command = Console.ReadLine();
+                    Console.Clear();
+
+                    switch (command)
+                    {
+                        case "list": ListAllProjects(connection); break;
+                        case "details": ShowProjectDetails(connection); break;
+                        case "search": SearchByNameFirst(connection); break;
+                        case "searchALL": SearchByNameAll(connection); break;
+                        case "exit": return;
+                    }
+                }
+            }
+        }
+
+        private static void ListAllProjects(SqlConnection connection)
+        {
+            string query = @"SELECT ProjectID, Name FROM Projects";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            using (reader)
+            {
+                Console.WriteLine("  ID | ProjectName");
+                Console.WriteLine("---- + --------------");
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader[0],4} | {reader[1]}");
+                }
+            }
+        }
+
+        private static void ShowProjectDetails(SqlConnection connection)
+        {
+            Console.Write("Enter ProjectID: ");
+            int projectID = int.Parse(Console.ReadLine());
+
+            string queryProjectDetails = @"
+                SELECT * FROM Projects 
+                WHERE ProjectID = @ProjectID";
+            SqlCommand cmd = new SqlCommand(queryProjectDetails, connection);
+            cmd.Parameters.AddWithValue("@ProjectID", projectID);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            // project details
+            using (reader)
+            {
+                if (!reader.Read())
+                {
+                    Console.WriteLine($"No project with ID {projectID} found");
+                    return;
+                }
+                Console.WriteLine("::::::::::::::::::::::::::::::::::::::");
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.WriteLine($"{reader.GetName(i),-15} : {reader[i]}");
+                }
+            }
+            // project employees
+            string queryEmployees = @"
+                SELECT e.EmployeeID, e.FirstName, e.LastName
+                FROM EmployeesProjects AS pe 
+                LEFT JOIN Employees AS e ON pe.EmployeeID = e.EmployeeID
+                WHERE pe.ProjectID = @ProjectID";
+            SqlCommand cmdEmpl = new SqlCommand(queryEmployees, connection);
+            cmdEmpl.Parameters.AddWithValue("@ProjectID", projectID);
+            SqlDataReader readerEmpl = cmdEmpl.ExecuteReader();
+
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("Assigned employees:");
+
+            using (readerEmpl)
+            {
+                if (!readerEmpl.HasRows)
+                { Console.WriteLine("No employees"); }
+                else
+                {
+                    while (readerEmpl.Read())
+                    {
+                        Console.WriteLine($"{readerEmpl[0],15} : {readerEmpl[1]} {readerEmpl[2]}");
+                    }
+                }
+                Console.WriteLine("::::::::::::::::::::::::::::::::::::::");
+            }
+        }
+
+        private static void SearchByNameAll(SqlConnection connection)
+        {
+            Console.Write("Enter project name: ");
+            string pattern = Console.ReadLine();
+
+            string query = @"
+                SELECT ProjectID FROM Projects 
+                WHERE Name LIKE @ProjectName";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@ProjectName", "%" + pattern + "%");
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            using (reader)
+            {
+                if (!reader.HasRows)
+                {
+                    Console.WriteLine("Project not found.");
+                    return;
+                }
+                List<int> projectIDs = new List<int>();
+                while (reader.Read())
+                {
+                    projectIDs.Add((int)reader[0]);
+                }
+                Console.WriteLine("Found projects with IDs: {0}", String.Join(", ", projectIDs));
+            }
+        }
+
+        private static void SearchByNameFirst(SqlConnection connection)
+        {
+            Console.Write("Enter project name: ");
+            string pattern = Console.ReadLine();
+
+            string query = @"
+                SELECT TOP 1 ProjectID FROM Projects 
+                WHERE Name LIKE @ProjectName";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@ProjectName", "%" + pattern + "%");
+            int? projectID = (int?)cmd.ExecuteScalar() ?? -1; // NB! nullable with default value if null
+
+            if (projectID == -1)
+            {
+                Console.WriteLine("Project not found.");
+                return;
+            }
+            Console.WriteLine($"First project found: ID {projectID}");
+        }
+    }
+}
